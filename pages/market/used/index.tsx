13d3f4545e0ca18c084d373ec496003c2260b792
jjmin321 <jjmin321@naver.com>
2021-04-26 12:25:47 +0900

    [ADD] /market/used page

---
+++ pages/market/used/index.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import UsedComponent from '../../../src/Components/market/used/Used';
+
+export default class CustomMarketPage extends Component {
+    render() {
+        return (
+            <UsedComponent/>
+        );
+    }
+}
---
+++ src/Components/market/used/Used.tsx
@@ -0,0 +1,9 @@
+const Used = () => {
+    return (
+        <>
+        <h1>중고 마켓입니다</h1>
+        </>
+    )
+}
+
+export default Used;
