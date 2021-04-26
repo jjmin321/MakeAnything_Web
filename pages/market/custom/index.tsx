9341289db0a79834337f57005800105a88d30328
jjmin321 <jjmin321@naver.com>
2021-04-26 12:23:03 +0900

    [ADD] /market/custom page

---
+++ pages/market/custom/index.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import CustomComponent from '../../../src/Components/market/custom/Custom';
+
+export default class CustomMarketPage extends Component {
+    render() {
+        return (
+            <CustomComponent/>
+        );
+    }
+}
---
+++ src/Components/market/custom/Custom.tsx
@@ -0,0 +1,8 @@
+const Custom = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default Custom;
