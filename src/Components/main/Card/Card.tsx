d4e85f3ea0d45fec0ced1237fc8f2687b6a19789
jjmin321 <jjmin321@naver.com>
2021-05-01 21:47:01 +0900

    [ADD] flex-wrap for card item

--- src/Components/main/Card/Card.tsx
+++ src/Components/main/Card/Card.tsx
@@ -22,45 +22,24 @@ const Card = () => {
 
   return (
     <>
-      <div className="Card-Description">
-        <div className="Card-Description-Start">재능 마켓이에요!</div>
-        <div
-          className="Card-Description-End"
-          onClick={() => (window.location.href = "/market/talent")}
-        >
-          {"<< 이동하기!"}
-        </div>
-      </div>
       <div className="Card">
         {talentItem.length > 0 &&
           talentItem.map((item: IItem, index: number) => (
             <CardItem key={index} item={item} />
           ))}
+          {usedItem.length > 0 &&
+          usedItem.map((item: IItem, index: number) => (
+            <CardItem key={index} item={item} />
+          ))}
       </div>
-      <div className="Card-Description">
-        <div className="Card-Description-Start">중고 마켓이에요!</div>
-        <div
-          className="Card-Description-End"
-          onClick={() => (window.location.href = "/market/used")}
-        >
-          {"<< 이동하기!"}
-        </div>
-      </div>
+      
       <div className="Card">
         {usedItem.length > 0 &&
           usedItem.map((item: IItem, index: number) => (
             <CardItem key={index} item={item} />
           ))}
       </div>
-      <div className="Card-Description">
-        <div className="Card-Description-Start">구매 요청 마켓이에요!</div>
-        <div
-          className="Card-Description-End"
-          onClick={() => (window.location.href = "/market/custom")}
-        >
-          {"<< 이동하기!"}
-        </div>
-      </div>
+      
       <div className="Card">
         {customItem.length > 0 &&
           customItem.map((item: IItem, index: number) => (
