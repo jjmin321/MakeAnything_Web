aecb094b47d7b444edf22bad855f52d6056b41cb
jjmin321 <jjmin321@naver.com>
2021-05-10 15:02:20 +0900

    [FIX] Can't resolve url-loader webpack error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import React, { Component, useState } from "react";
+import "semantic-ui-css/semantic.min.css";
 import HomeComponent from "../src/Components/main/Home"
 
 export default class MainPage extends Component {
