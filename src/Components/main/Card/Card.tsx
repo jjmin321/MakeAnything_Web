cea0370bc88641664c2652d240f012a1aee56080
jjmin321 <jjmin321@naver.com>
2021-05-13 09:00:32 +0900

    [IMPROVE] Item Sort Dropdown CSS

--- src/Components/main/Card/Card.tsx
+++ src/Components/main/Card/Card.tsx
@@ -9,11 +9,11 @@ const Card = () => {
   const [Item, setItem] = useState<Array<IItem>>([]);
 
   const onChange = (data) => {
-    console.log(`/item/${data.value}`)
-      customAxios.get(`/item/${data.value}`).then(({ data }) => {
-        setItem(data.data);
-        console.log("asdf");
-      });
+    console.log(`/item/${data.value}`);
+    customAxios.get(`/item/${data.value}`).then(({ data }) => {
+      setItem(data.data);
+      console.log("asdf");
+    });
   };
 
   useLayoutEffect(() => {
@@ -27,20 +27,23 @@ const Card = () => {
 
   return (
     <>
-      <Dropdown
-        className="sort"
-        onChange={(e, data) => onChange(data)}
-        text="물품 정렬 ▼"
-        options={[
-          { key: 1, text: "신규 등록 순", value: "getAllItemsDesc" },
-          { key: 2, text: "예전 등록 순", value: "getAllItems" },
-          { key: 3, text: "가격 높은 순", value: "getAllItemsByPriceDesc" },
-          { key: 4, text: "가격 낮은 순", value: "getAllItemsByPrice" },
-          { key: 5, text: "재능 물품만", value: "getTalentItems" },
-          { key: 6, text: "중고 물품만", value: "getUsedItems" },
-          { key: 7, text: "판매 요청 물품만", value: "getCustomItems" },
-        ]}
-      />
+      <div className="Sort">
+        <button className="Sort-Dropdown">
+          <Dropdown
+            onChange={(e, data) => onChange(data)}
+            text="물품 정렬 순▼"
+            options={[
+              { key: 1, text: "신규 등록 순", value: "getAllItemsDesc" },
+              { key: 2, text: "예전 등록 순", value: "getAllItems" },
+              { key: 3, text: "가격 높은 순", value: "getAllItemsByPriceDesc" },
+              { key: 4, text: "가격 낮은 순", value: "getAllItemsByPrice" },
+              { key: 5, text: "재능 물품만", value: "getTalentItems" },
+              { key: 6, text: "중고 물품만", value: "getUsedItems" },
+              { key: 7, text: "판매 요청 물품만", value: "getCustomItems" },
+            ]}
+          />
+        </button>
+      </div>
       <div className="Card">
         {Item.length > 0 &&
           Item.map((item: IItem, index: number) => (
